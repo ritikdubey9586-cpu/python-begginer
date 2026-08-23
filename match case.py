d2af95f471ac3b530d6b1bce05a616bce7cace4e
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-08-23 22:27:54 +0530

    match_case added

---
+++ match case.py
@@ -0,0 +1,17 @@
+# match case is similar to switch case as in clanguage.
+
+num=int(input("enter a number :"))
+
+match num:
+
+         case 1:
+                print("1")
+         case 2:
+                print("2")
+         case 3:
+                print("3") 
+         case _:
+                print("not in range") 
+
+ # here _ this means default case .
+
