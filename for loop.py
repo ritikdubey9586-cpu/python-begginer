a913a066e884e7c05f86b7d8fbdb14983156a33a
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-08-26 01:17:34 +0530

    for_loop_added

--- for loop.py
+++ for loop.py
@@ -26,5 +26,7 @@ for a in range(1,n):
 
 
 
+# break is used to stop the loop.
+
 # n is for take input from users for example if user typr 10000
 # so for loop will start from 1 as we declare start from 1 and end will whatever users put input so here user is put the last value 10000 so as for loop there is should print of 1 to 10000 but sorry only print 1 to 2500 because when 2500 come loop will stop due to break statement and at 1500 and 2000 it will print the number is 1500 and number is 2000 but loop will not end loop will continue ok when it comes at 2500 then loop will stop due to break statement. 
