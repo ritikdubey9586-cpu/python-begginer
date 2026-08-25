2fd8473484853de2b388a5cc21a6efb37b53366e
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-08-26 01:10:54 +0530

    function_added

---
+++ function arguments.py
@@ -0,0 +1,11 @@
+def average(a,b):
+
+ a=int(input("enter any value of a : "))
+ b=int(input("enter any value of b : "))
+
+print("the average of a and b " , ( int(a) + int(b) ) / 2)
+    
+#average(5,6)
+
+
+# basically when we write average to get output of our user defined function then answer will come but we can change the argument also ok and we can change augument any one also.
---
+++ function.py
@@ -0,0 +1,40 @@
+def additionof(a,b):
+    c=a+b
+    print(c)
+
+def subtraction(a,b): 
+    c=a-b
+    print(c)
+
+def multiplication(a,b):
+    c=a*b
+    print(c) 
+
+def division(a,b):
+    c=a/b
+    print(c)
+
+a=int(input("enter  number for the value of a :  "))
+b=int(input("enter  number for the value of b :  "))
+
+additionof(a,b)
+subtraction(a,b)
+multiplication(a,b)
+division(a,b)
+
+def isgreater(a,b):
+    if (a>b):
+     print("a is greater")
+    else:
+        print("b is greater")
+
+isgreater(a,b)
+
+def ritikbhai(a,b):
+    pass
+
+# here basicaaly we define a function and we can use it only just write whatever we give the name 
+# basically we know that when we put data entry in file with some name and rool no. and if we want to write same file sheet then we write again whole code so for this reason we declare the function when we make function then we can use it anytime 
+# and when there is no need of some function then we can use pass ok.
+
+
