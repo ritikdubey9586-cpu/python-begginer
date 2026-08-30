12330f733a59c5d4078b0e0bb9394024a0d94686
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-08-30 23:53:46 +0530

    print_changes

--- main1.py
+++ main1.py
@@ -1,2 +1,14 @@
 print("my name is ritik dubey ")
 print("my hobby is play a batminton")
+
+
+
+print(" my name is airor-1 ")
+print(" my name is airor-8 ")
+print(" my name is airor-2 ")
+print(" my name is airor-3 ")
+print(" my name is airor-4 ")
+print(" my name is airor-5 ")
+print(" my name is airor-6 ")
+print(" my name is airor-7 ")
+print(" my name is airor-9 ")
