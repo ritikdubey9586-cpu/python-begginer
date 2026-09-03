0607c5e812099028dd43512f8fafb8633f16073f
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-03 23:58:03 +0530

    new_code_added

--- currenttime.py
+++ currenttime.py
@@ -1,18 +1,27 @@
-import time
-t=time.strftime('%H:%M:%S')
+import time 
+
+t=(time.strftime("%H:%M:%S"))
 print(t)
 
-hour=int(time.strftime('%H'))
-hour=int(input("enter the time you want: "))
+# this is for what time exactly in your pc live ok 
+
+# now we write a code for take users input if users type any time then he or she will get the welcome message whaterver time he or she mentioned .
+
+hour=time.strftime("%H")
+hour=int(input("Enter the hour in 24 hour format: "))
 print(hour)
 
-if (hour>=0 and hour<12):
-    print("Good Morning")
-elif (hour>=12 and hour<16):
-    print("Good Afternoon")
-elif (hour>=16 and hour<20):
-    print("Good Evening")
-else:
-    print("Good Night")
 
-print("Thank you for visiting!")
+if (hour>=0 and hour<12):
+    print("good morning")
+elif (hour>=12 and hour<16):
+    print("good afternoon")
+elif (hour>=16 and hour<20):
+    print("good evening")
+elif (hour>=20 and hour<24):
+    print("good night:")
+else:
+    print("no answer ! ")
+
+
+
