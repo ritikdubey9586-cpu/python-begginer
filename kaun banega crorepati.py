5df006d63c0216db5f226c44b521c1fea4c7ab23
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-04 23:51:45 +0530

    kaun_banega_crorepati

---
+++ kaun banega crorepati.py
@@ -0,0 +1,19 @@
+# actually we made a program which is in kaon banega crorepati 
+
+print("Welcome to Kaun Banega Crorepati!")
+
+print("what is your name ?")
+list = ["(a) amit", "(b) ankit", "(c) anshul", "(d) anshika"]
+
+name = input("choose correct answer from the list and write :")
+
+
+if (name == "amit"):
+    a=print("you won 1 crore ")
+    print("you are eligible for next question  ")
+    print("total balance = 1 crore ")
+else:
+    print("you lost!")
+    print("current balance =0")
+
+    
