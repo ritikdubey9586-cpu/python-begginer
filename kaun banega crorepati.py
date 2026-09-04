e200adca562b4eea1291952fc13a2dc3acf80fb1
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-04 23:59:35 +0530

    added_changes

--- kaun banega crorepati.py
+++ kaun banega crorepati.py
@@ -1,5 +1,8 @@
 # actually we made a program which is in kaon banega crorepati 
 
+from matplotlib.pyplot import show
+
+
 print("Welcome to Kaun Banega Crorepati!")
 
 print("what is your name ?")
@@ -9,11 +12,29 @@ name = input("choose correct answer from the list and write :")
 
 
 if (name == "amit"):
-    a=print("you won 1 crore ")
+    print("you won 1 crore ")
     print("you are eligible for next question  ")
     print("total balance = 1 crore ")
 else:
     print("you lost!")
     print("current balance =0")
 
-    
+show = input("do you want to play again ? (yes/no) : ")
+show()
+
+if (show == "yes"):
+    print("Welcome to Kaun Banega Crorepati!")
+
+    print("what is your pet name ?")
+    list = ["(a) amit", "(b) ankit", "(c) anshul", "(d) anshika"]
+
+    name = input("choose correct answer from the list and write :")
+
+
+    if (name == "amit"):
+        print("you won 1 crore ")
+        print("you are eligible for next question  ")
+        print("total balance = 1 crore ")
+    else:
+        print("you lost!")
+        print("current balance =0")
