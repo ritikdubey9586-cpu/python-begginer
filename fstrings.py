a1f5f4f2448f8766b3049e9b5761f8bef602a3b7
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-06 02:29:34 +0530

    fstrings_added

---
+++ fstrings.py
@@ -0,0 +1,18 @@
+a = "ritik"
+b = "ankit"
+
+print(f"My name is {a} and my friend's name is {b}.")
+
+
+# this is a simple example of using f-strings in Python to format strings with variables. The output will be:
+
+# this code is used to add any variables to a string in a readable and efficient manner.
+
+
+# if we want to not add the value of a and b so simply use duble curly braces {{}} to escape the variable and print it as a string.
+
+# example for it 
+
+print("my name is {{a}} and my friend's name is {{b}}.")
+
+
