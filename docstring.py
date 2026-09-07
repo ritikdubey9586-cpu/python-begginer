fd3e0ccadbe8705d9ad28d9acaa687459d3b960b
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-07 23:49:00 +0530

    docstring_added

---
+++ docstring.py
@@ -0,0 +1,12 @@
+def square(num):
+    """This function takes a number as input and returns the square of that number."""
+    return num ** 2
+
+print(square(5))  # Output: 25
+print(square.__doc__)  # Output: This function takes a number as input and returns the square of that number.
+
+# basically we used """ """ this is used for doc string purpose and we can used as a comment also but when we not write doc string then we can used ok
+
+
+
+
