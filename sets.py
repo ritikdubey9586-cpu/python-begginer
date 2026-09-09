bdd3b191796d8a825309ae9d882e845e063f607b
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-09 23:18:30 +0530

    sets_added

---
+++ sets.py
@@ -0,0 +1,20 @@
+name = { 1,2,3,5,6,7,"ritik" , 8 , 4,4,3}
+
+print(name)
+
+# sets never print duplicate values and it is unordered collection of data type.
+
+# sets is always written in curly brackets.
+
+# if we want to write empty sets 
+
+# info = {} # this will print dictionary because it is empty dictionary.
+# print(type(info)) # this will print dictionary because it is empty dictionary.
+
+info = set()
+print(type(info)) # this will print set because it is an empty set.
+
+# in sets we cant display any number as we can display in tuple 
+# for example
+
+print(name[0]) # this will give error because sets are unordered collection of data type.
