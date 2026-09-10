cb599716db889152443edbcfe78c01d3d8c38d38
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-10 23:33:32 +0530

    methods_of _set_added

---
+++ setsmethod.py
@@ -0,0 +1,21 @@
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+print(city1.union(city2))
+
+
+# union is used to combine differenet different sets to make single sets.
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+print(city1.intersection(city2))
+
+
+
+
+# intersection is used to make sets without same value either in two sets or more than two sets means it is used to find the common elements from sets.
+
+
+
+
+
