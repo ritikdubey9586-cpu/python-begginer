87e030235c74650875bc2f81d61decb0dd78de8a
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-10 23:44:20 +0530

    more_methods_added

--- setsmethod.py
+++ setsmethod.py
@@ -8,14 +8,70 @@ print(city1.union(city2))
 
 city1={"surat","kim","vadodara","rajkot","ahmedabad"}
 city2={"mumbai","delhi","bangalore","chennai","kolkata"}
-print(city1.intersection(city2))
-
-
-
+cities=city1.intersection(city2)
+print(cities)
 
 # intersection is used to make sets without same value either in two sets or more than two sets means it is used to find the common elements from sets.
 
 
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+cities=city1.intersection_update(city2)
+print(cities)
 
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+cities=city1.difference(city2)
+print(cities)
+
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+cities=city1.isdisjoint(city2)
+print(cities)
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+cities=city1.issuperset(city2)
+print(cities)
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+cities=city1.issubset(city2)
+print(cities)
+
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+
+city1.add("amit")
+print(city1)
+
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+
+city1.remove("surat")
+print(city1)
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+
+city1.discard("amit")
+print(city1)
+
+
+city1={"surat","kim","vadodara","rajkot","ahmedabad"}
+city2={"mumbai","delhi","bangalore","chennai","kolkata"}
+
+a=city1.pop()
+print(a)
 
 
