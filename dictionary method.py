79a0a5f699f694b651b6f791f2ceb7667ec5f119
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-15 23:51:09 +0530

    method_dictionary_added

---
+++ dictionary method.py
@@ -0,0 +1,19 @@
+name = { "ritik" : 10 , "ankit" : 20 , "sonam" : 30}
+surname = { "pandey" : 40 , " dubey " : 50 , " mishra " : 30}
+
+name.update(surname)
+print(name)
+
+
+
+name.clear()
+print(name)
+
+
+
+name.pop("ritik")
+print(name)
+
+
+name.popitem()
+print(name)
