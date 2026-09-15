ce3f180046b000cd0808ea1bc929fc2ab3f84d29
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-15 23:35:09 +0530

    dictionary_added

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+name = { 10 : "ritik" , "ankit" : 20 , "sonam" : 30 }
+print (name)
+
+print(name.keys())
+# this is used for dictionary but it will tell first one    
+
+print(name.values())
+# this is used for dictionary but it will tell first one  of values ok .
+
+print(name.items())
+
+# this is  also used for dictionary but it will tell whole dictionary ok.
+
+
