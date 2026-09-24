1fb85161a3fee87280433b7957286507e91e6e40
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-09-24 23:43:38 +0530

    added_coding_decoding

---
+++ conversion of normal code into secretcode.py
@@ -0,0 +1,18 @@
+# write a  python program in which message will convert into secret code language.  Use the rules which is mention below if you want to convert normal english into secret code language. 
+
+
+#coding 
+# if the word contains atleast 3 character then remove first letter and append it end of the word  and then append (add) random three letter at starting of the letter 
+# else :
+#          simply reverse the letter means if word is less than 3 then simply reverse the letters.
+
+
+#decoding 
+# if the words contains less than 3 letter then simply reverse it 
+# else :
+# remove 3 letter from starting and ending then remove the last letter and append that letter at the starting of the words . 
+
+
+
+
+# by using these technique we can easily do coding and decoding 
